58bbcf84fab44c995629ad609fde345926a26723
amrit2k-droid <amritkumar045@gmail.com>
2021-04-24 14:57:16 +0530

    itemlist

--- App.js
+++ App.js
@@ -1,12 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Unorderedlist from 'react-native-unordered-list';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [items, setItems] = useState(["Milk", "Coffee", "Oranges"])
+  const [filteredItems, setFilteredItems] = useState(["Milk", "Coffee", "Oranges"]);
+  const [addedItems, setAddedItems] = useState([]);
+  const [btnClicked, setBtnClicked] = useState(false);
+  const [text, setText] = useState('')
+  const [searched, setSearched] = useState(false);
+
+  const searchItemHandler = (textInput) => {
+  if(!btnClicked) {
+    if(textInput) {
+      const data = items.filter(item => {
+        return item.toLowerCase().includes(textInput.toLowerCase())
+      })
+      setFilteredItems(data);
+      setText(textInput);
+    }
+    else {
+      setFilteredItems(items);
+      setText(textInput)
+    }
+  }
+  else {
+    if(textInput) {
+      const data = addedItems.filter(item => {
+        return item.toLowerCase().includes(textInput.toLowerCase())
+      })
+      const items1 = items.filter(item => {
+        return item.toLowerCase().includes(textInput.toLowerCase())
+      })
+      setFilteredItems(data.concat(items1));
+      setText(textInput);
+    }
+    else {
+      setFilteredItems(addedItems.concat(items));
+      setText(textInput);
+    }
+  }
+
+  setSearched(true);
+}
+
+const addItemHandler = () => {
+//  setAddedItems(addedItems.concat( Math.random().toString(36).substring(7)))
+  setBtnClicked(true);
+  setSearched(false)
+  setAddedItems(addedItems.concat( Math.random().toString(36).substring(7)));
+//  setAddedItems(addedItems.concat(filteredItems));
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.parentView}>
+        <TextInput 
+          placeholder = "Search"
+          onChangeText = {(text) => searchItemHandler(text)}
+          value = {text}
+          style = {styles.search}
+        />
+        <View style={styles.buttonView}>
+          <Button
+            onPress = {() => addItemHandler()}
+            title = "+"
+          />
+        </View>
+        <Unorderedlist>
+            {searched ? filteredItems.map(item => (
+              <Text key={item}  style={styles.list}>{item}</Text>
+            )) : addedItems.concat(items).map(item1 => (
+              <Text style={styles.list}>{item1}</Text>
+            ))}
+          </Unorderedlist>
+      </View>
     </View>
   );
 }
@@ -16,6 +85,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  parentView: {
+    backgroundColor: 'lightpink',
+    width: 400,
+    height: 500,
+    marginTop: 35,
+    borderRadius: 4
+  },
+  search: {
+   alignSelf: 'flex-start',
+   backgroundColor: 'gray',
+   width: 200,
+   borderRadius: 2,
+   marginLeft: 75,
+   height: 35,
+   marginTop: 20
+   
+  },
+  buttonView: {
+   width: 50,
+   alignSelf: 'flex-end',
+   marginRight: 70,
+   marginTop: -35,
+
+},
+list: {
+  marginLeft: 150,
+  paddingTop: 20,
+  width: 100
+}
 });
